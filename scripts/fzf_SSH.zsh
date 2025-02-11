89f34e0123975f9a72e33a3e20da0327768ebe32
heydaytime <whynotmihir@gmail.com>
2025-02-11 10:16:21 -0600

    ssh script works without IdentityFile

--- scripts/fzf_SSH.zsh
+++ scripts/fzf_SSH.zsh
@@ -3,31 +3,31 @@
 # File path to your SSH config
 SSH_CONFIG=~/.ssh/config
 
-# Parse the SSH config to extract Host, HostName, and IdentityFile
-commands=$(awk '
-  /^Host / { 
-    host = $2 
+
+commands=$(
+awk '
+  /^Host / {
+    host = $2
   }
-  /HostName / { 
-    hostname = $2 
-  }
-  /IdentityFile / { 
-    identity = $2 
-    if (host && hostname && identity) {
-      print "ssh -i " identity " " host "@" hostname
+  /HostName/ {
+    hostname = $2
+    if (host && hostname) {
+      command = "ssh " host "@" hostname
+      print command
       host = ""
       hostname = ""
-      identity = ""
     }
   }
 ' "$SSH_CONFIG")
 
 # Use fzf to let the user select a command, and then execute it
 selected_command=$(echo "$commands" | fzf)
-
 # If a command is selected, execute it
 if [[ -n "$selected_command" ]]; then
+    server_name=$(echo "$selected_command" | awk -F'@' '{print $2}')
+    eval tmux rename-window "$server_name"
     eval "$selected_command"
+    eval tmux kill-window
 else
    tmux kill-window 
 fi
